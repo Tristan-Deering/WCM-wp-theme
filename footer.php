f87cef7bb54d90ebbedcacb39dff3bd4d1b1dc0d
Noonmoon <tristandeering@hotmail.com>
2019-09-09 17:28:49 -0600

    footer styling and markup

--- footer.php
+++ footer.php
@@ -11,24 +11,45 @@
 
 ?>
 
-	</div><!-- #content -->
-
 	<footer id="footer">
-		<div class="container">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wcmcbd' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'wcmcbd' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'wcmcbd' ), 'wcmcbd', '<a href="http://underscores.me/">Tristan Deering</a>' );
-				?>
-		</div><!-- .site-info -->
+		<div class="footer-container">
+			<div class="top-footer d-flex justify-content-between">
+				<img src="<?php bloginfo('template_directory'); ?>/assets/images/logo.png" alt="" class="d-inline-block img-fluid logo">
+				<span>
+					Need to contact us?   
+					<button class="d-inline-block hero-cta-button">Press me</button>
+				</span>
+			</div>
+			<div class="lower-footer row">
+				<div class="col-2 footer-menu">
+					<p class="link-heading">Navigation</p>
+					<a href="/" class="footer-link">Home</a>
+					<a href="/shop" class="footer-link">Store</a>
+					<a href="/shop" class="footer-link">CBD Research</a>
+					<a href="/shop" class="footer-link">Wholesale</a>
+					<a href="/shop" class="footer-link">Blog</a>
+				</div>
+				<div class="col-2 footer-menu">
+					<p class="link-heading">Company</p>
+					<a href="/shop" class="footer-link">About Us</a>
+					<a href="/shop" class="footer-link">WCM Identity</a>
+					<a href="/shop" class="footer-link">Terms & Conditions</a>
+					<a href="/shop" class="footer-link">Privacy Policy</a>
+				</div>
+				<div class="col-5 footer-menu">
+					<p class="link-heading">Support</p>
+					<a href="/shop" class="footer-link">Contact</a>
+					<a href="/shop" class="footer-link">Live Chat</a>
+				</div>
+				<div class="col-3 footer-menu">
+					<p class="link-heading">Connect With Us</p>
+					<a href="/shop" class="footer-link">Facebook</a>
+					<a href="/shop" class="footer-link">Instagram</a>
+				</div>
+			</div>
+		</div><!-- .container -->
 	</footer><!-- #colophon -->
-</div><!-- #page -->
+</div><!-- #page div -->
 
 <?php wp_footer(); ?>
 
--- front-page.php
+++ front-page.php
@@ -37,40 +37,38 @@
     </div>
   </section>
 
-  <section class="content-section container">
+  <section class="content-section container container-lg">
     <div class="row">
 
-      <div class="col-6">
+      <div class="col-6 rmv-padding">
         <img src="<?php bloginfo('template_directory'); ?>/assets/images/blob-caps-display.png" class="img-fluid" alt="">
       </div>
 
       <div class="col-6">
         <h2 class="h2-xl header-xl">About</h2>
+      
+        <div class="tabs-wrapper">
 
-        <!-- Nav tabs -->
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
-          <li class="nav-item">
-            <a class="tabs nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="tabs nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
-          </li>
-          <li class="nav-item">
-            <a class="tabs nav-link" id="messages-tab" data-toggle="tab" href="#messages" role="tab" aria-controls="messages" aria-selected="false">Messages</a>
-          </li>
-          <li class="nav-item">
-            <a class="tabs nav-link" id="settings-tab" data-toggle="tab" href="#settings" role="tab" aria-controls="settings" aria-selected="false">Settings</a>
-          </li>
-        </ul>
-
-        <!-- Tab panes -->
-        <div class="tab-content">
-          <div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
-          <div class="tab-pane" id="profile" role="tabpanel" aria-labelledby="profile-tab">..s.</div>
-          <div class="tab-pane" id="messages" role="tabpanel" aria-labelledby="messages-tab">..d.</div>
-          <div class="tab-pane" id="settings" role="tabpanel" aria-labelledby="settings-tab">..f.</div>
+          <!-- Circle tabs -->
+          <ul class="nav nav-tabs" id="circle-tabs-menu" role="tablist">
+            <li class="circle-tab"><a class="tab-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">
+              <div class="tab-wrapper">hello</div>
+            </a></li>
+            <li class="circle-tab"><a class="tab-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">
+              <div class="tab-wrapper">hello</div>
+            </a></li>
+            <li class="circle-tab"><a class="tab-link" id="messages-tab" data-toggle="tab" href="#messages" role="tab" aria-controls="messages" aria-selected="false">
+              <div class="tab-wrapper">hello</div>
+            </a></li>
+          </ul>                     
+  
+          <!-- Tab panes -->
+          <div class="tab-content">
+            <div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+            <div class="tab-pane" id="profile" role="tabpanel" aria-labelledby="profile-tab">..s.</div>
+            <div class="tab-pane" id="messages" role="tabpanel" aria-labelledby="messages-tab">..d.</div>
+          </div>
         </div>
-
       </div>
     </div>
   </section>
